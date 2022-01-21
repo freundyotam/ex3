ebad3202b1fb1ce2fafa650a3d65e832d55bfcf3
yotam freund <freund.yotam@gmail.com>
2022-01-21 16:10:41 +0200

    Add check match function

--- hw3_part2.py
+++ hw3_part2.py
@@ -2,6 +2,10 @@ def get_all_sub_str(string, length):
     return [string[i:i+length] for i in range(len(string) - length + 1)]
 
 
+def get_all_indexes(string, char):
+    return set([i for i in range(len(string)) if string[i] == char])
+
+
 def get_palindrom_dict(string):
     final_dict = {}
     for i in range(1, len(string) + 1):
@@ -9,3 +13,16 @@ def get_palindrom_dict(string):
         if palindroms:
             final_dict[i] = palindroms
     return final_dict
+
+
+def check_match(string):
+    first_str = string[::2]
+    second_str = string[1::2]
+    if len(first_str) != len(second_str):
+        return False
+
+    for i in range(len(first_str)):
+        if not get_all_indexes(first_str, first_str[i]).issubset(get_all_indexes(second_str, second_str[i])):
+            return False
+
+    return True
